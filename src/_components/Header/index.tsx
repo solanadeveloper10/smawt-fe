180e6e105fdd2046445c0888a60e7492778186b0
solanadeveloper10 <solanadeveloper10@gmail.com>
2025-01-17 15:33:50 +0200

    create burger menu

--- src/_components/Header/index.tsx
+++ src/_components/Header/index.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { motion } from "motion/react"
 
 import styles from './styles.module.scss'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
@@ -13,36 +20,56 @@ const Header = () => {
         >
           <img src="assets/images/logo.png" alt="Smawt logo" className={styles.logo} />
         </motion.a>
-        <motion.a href="/"
-          whileHover={{
-            scale: 1.1,
-            color: 'red',
-          }}
-        >Telegram</motion.a>
-        <motion.a href="/"
-          whileHover={{
-            scale: 1.1,
-            color: 'red',
-          }}
-        >Twitter</motion.a>
-        <motion.a href="/"
-          whileHover={{
-            scale: 1.1,
-            color: 'red',
-          }}
-        >Screener</motion.a>
-        <motion.a href="/"
-          whileHover={{
-            scale: 1.1,
-            color: 'red',
-          }}
-        >Dextools</motion.a>
-        <motion.a href="/"
-          whileHover={{
-            scale: 1.1,
-            color: 'red',
-          }}
-        >Buy</motion.a>
+        <ul className={isMenuOpen ? `${styles.navLinks} ${styles.open}` : styles.navLinks}>
+          <li>
+            <motion.a href="/"
+              whileHover={{
+                scale: 1.1,
+                color: 'red',
+              }}
+            >Telegram</motion.a>
+          </li>
+          <li>
+            <motion.a href="/"
+              whileHover={{
+                scale: 1.1,
+                color: 'red',
+              }}
+            >Twitter</motion.a>
+          </li>
+          <li>
+            <motion.a href="/"
+              whileHover={{
+                scale: 1.1,
+                color: 'red',
+              }}
+            >Screener</motion.a>
+          </li>
+          <li>
+            <motion.a href="/"
+              whileHover={{
+                scale: 1.1,
+                color: 'red',
+              }}
+            >Dextools</motion.a>
+          </li>
+          <li>
+            <motion.a href="/"
+              whileHover={{
+                scale: 1.1,
+                color: 'red',
+              }}
+            >Buy</motion.a>
+          </li>
+        </ul>
+        <div
+          className={`${styles.burgerMenu} ${isMenuOpen ? styles.open : ""}`}
+          onClick={toggleMenu}
+        >
+          <span className={styles.line}></span>
+          <span className={styles.line}></span>
+          <span className={styles.line}></span>
+        </div>
       </nav>
     </header>
   )
